fe4c5edd3f717e4efc178294b1670ebe5b8b878e
Sudipto Das <shiningsudipto@gmail.com>
2023-06-08 02:37:23 +0600

    storing selected class

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ app.get('/', (req, res) => {
     res.json('Megapixel IS Running')
 })
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kh5m3gl.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -32,13 +49,29 @@ async function run() {
         await client.connect();
 
         const classCollection = client.db('megapixel').collection('classes');
+        const selectedClassCollection = client.db('megapixel').collection('selectedClasses');
         const userCollection = client.db('megapixel').collection('users');
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            res.send({ token });
+        })
+        // all classes
         app.get('/classes', async (req, res) => {
             const classes = classCollection.find();
             const result = await classes.toArray();
             res.send(result);
         })
+        // storing selected classes
+        app.post('/selectedClass', async (req, res) => {
+            const item = req.body;
+            const result = await selectedClassCollection.insertOne(item);
+            res.send(result)
+        })
+        // all instructors
         app.get('/instructors', async (req, res) => {
             const result = await userCollection.find({ role: 'instructor' }).toArray();
             res.send(result);
