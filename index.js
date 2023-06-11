c6e0eb6503672eeab707a649f1a48b4834f51b96
Sudipto Das <shiningsudipto@gmail.com>
2023-06-12 04:56:17 +0600

    verify done

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -11,7 +10,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.json('Megapixel IS Running')
@@ -49,7 +47,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const classCollection = client.db('megapixel').collection('classes');
         const selectedClassCollection = client.db('megapixel').collection('selectedClasses');
@@ -63,18 +61,75 @@ async function run() {
             })
             res.send({ token });
         })
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            if (user?.role !== 'Admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next()
+        }
+        const verifyInstructor = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            if (user?.role !== 'Instructor') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next()
+        }
+        // Identifying admin or not
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            // console.log(email);
+            // console.log(req.decoded.email)
+            if (req.decoded.email !== email) {
+                return res.json({ admin: false })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            // console.log(user);
+            const result = { admin: user?.role === 'Admin' }
+            res.json(result)
+        })
+        // Identifying instructor or not
+        app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            // console.log(email);
+            // console.log(req.decoded.email);
+            if (req.decoded.email !== email) {
+                return res.json({ instructor: false })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            // console.log(user);
+            const result = { instructor: user?.role === 'Instructor' }
+            res.json(result)
+        })
+        app.get('/users/student/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                return res.json({ student: false })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            const result = { student: user?.role === 'student' }
+            res.json(result)
+        })
+
         // all classes
-        app.get('/manageClasses', async (req, res) => {
+        app.get('/manageClasses', verifyJWT, verifyAdmin, async (req, res) => {
             const classes = await classCollection.find().toArray();
-            res.send(classes);
+            res.json(classes);
         });
         // all classes with status 'Approved'
         app.get('/classes', async (req, res) => {
-            const classes = await classCollection.find({ status: "Approved" }).toArray();
-            res.send(classes);
+            const classes = await classCollection.find({ status: "Approved" }).sort({ availableSeats: 1 }).toArray();
+            res.json(classes);
         });
         // Making class status pending to approved and Deny
-        app.put('/classes/approve/:id', async (req, res) => {
+        app.put('/classes/approve/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const updateStatus = req.body;
@@ -89,12 +144,12 @@ async function run() {
                 };
             }
             const result = await classCollection.updateOne(query, update);
-            res.send(result)
+            res.json(result)
         })
         // setting feedback to the class
         app.patch('/classes/feedback/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
+            // console.log(id);
             const query = { _id: new ObjectId(id) };
             const feedback = req.body.feedback; // Retrieve the feedback value from the request body
             const update = { $set: { feedback: feedback } }
@@ -102,15 +157,15 @@ async function run() {
             res.json(updatedClass);
         });
         // storing selected classes
-        app.post('/selectedClass', async (req, res) => {
+        app.post('/selectedClass', verifyJWT, async (req, res) => {
             const item = req.body;
             const result = await selectedClassCollection.insertOne(item);
-            res.send(result)
+            res.json(result)
         })
         // all instructors
         app.get('/instructors', async (req, res) => {
             const result = await userCollection.find({ role: 'Instructor' }).toArray();
-            res.send(result);
+            res.json(result);
         });
         // storing new user's information
         app.post('/newUser', async (req, res) => {
@@ -118,24 +173,24 @@ async function run() {
             const query = { email: user.email }
             const existingUser = await userCollection.findOne(query);
             if (existingUser) {
-                return res.send({ message: 'user already exists' })
+                return res.json({ message: 'user already exists' })
             }
             const result = await userCollection.insertOne(user);
-            res.send(result);
+            res.json(result);
         });
         // getting selected class by specific email
-        app.get('/myclass/:email', async (req, res) => {
+        app.get('/myclass/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const result = await selectedClassCollection.find({ studentEmail: email }).toArray();
             res.json(result);
         });
         // delete selected class
-        app.delete('/deleteSelectedClass/:id', async (req, res) => {
+        app.delete('/deleteSelectedClass/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             // console.log(id);
             const query = { _id: new ObjectId(id) }
             const result = await selectedClassCollection.deleteOne(query);
-            res.send(result);
+            res.json(result);
         })
         // getting selected class by id
         app.get('/findSelectedClass/:id', async (req, res) => {
@@ -143,7 +198,7 @@ async function run() {
             // console.log(id);
             const query = { _id: new ObjectId(id) }
             const result = await selectedClassCollection.findOne(query);
-            res.send(result);
+            res.json(result);
         })
         // payment intent
         app.post("/create-payment-intent", verifyJWT, async (req, res) => {
@@ -154,7 +209,7 @@ async function run() {
                 currency: 'usd',
                 payment_method_types: ['card']
             });
-            res.send({
+            res.json({
                 clientSecret: paymentIntent.client_secret
             })
         })
@@ -166,66 +221,66 @@ async function run() {
             const query = { _id: new ObjectId(id) }; // Match the classId field
             const deleteResult = await selectedClassCollection.deleteOne(query);
 
-            res.send({ insertResult, deleteResult });
+            res.json({ insertResult, deleteResult });
         });
         // updating available seats
         app.put('/updateavailableseats/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
+            // console.log(id);
             const query = { _id: new ObjectId(id) }
-            console.log(query);
+            // console.log(query);
             const classDocument = await classCollection.findOne(query);
             if (!classDocument) {
                 // Handle the case when the document is not found
                 return res.status(404).send({ error: "Class not found" });
             }
             const updateSeats = parseInt(classDocument.availableSeats) - 1;
-            console.log(classDocument.availableSeats);
+            // console.log(classDocument.availableSeats);
             const updateQuery = {
                 $set: {
                     availableSeats: updateSeats
                 }
             }
             const updateResult = await classCollection.updateOne(query, updateQuery)
-            res.send(updateResult)
+            res.json(updateResult)
         })
         // getting all enrolled class by email
-        app.get('/myEnrolledClass/:email', async (req, res) => {
+        app.get('/myEnrolledClass/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const result = await enrolledClassCollection.find({ studentEmail: email }).toArray();
             res.json(result);
         });
         // storing class added by the instructor
-        app.post('/instructorAddedClasses', async (req, res) => {
+        app.post('/instructorAddedClasses', verifyJWT, async (req, res) => {
             const query = req.body;
             const result = await classCollection.insertOne(query);
-            res.send(result)
+            res.json(result)
         })
         // getting added classes by instructor with email
-        app.get('/instructorsAddedClass/:email', async (req, res) => {
+        app.get('/instructorsAddedClass/:email', verifyJWT, verifyInstructor, async (req, res) => {
             const email = req.params.email;
             try {
                 const classes = await classCollection.find({ instructorEmail: email }).toArray();
-                res.send(classes);
+                res.json(classes);
             } catch (error) {
                 res.status(500).send('An unexpected error occurred.');
             }
         });
         // getting all registered user
-        app.get('/allRegisteredUsers', async (req, res) => {
+        app.get('/allRegisteredUsers', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
-            res.send(result)
+            res.json(result)
         })
         // getting user by email for identify role
-        app.get('/userRole/:email', async (req, res) => {
+        app.get('/userRole/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            console.log(email);
+            // console.log(email);
             const query = { email: email }
             const result = await userCollection.findOne(query);
-            res.send(result);
+            res.json(result);
         })
         // updating user role
-        app.put('/users/role/:id', async (req, res) => {
+        app.put('/users/role/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const updateRole = req.body.newRole;
@@ -235,8 +290,15 @@ async function run() {
                 }
             };
             const result = await userCollection.updateOne(query, update);
-            res.send(result);
+            res.json(result);
         });
+
+        app.get('/enrolledClass/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const query = { instructorEmail: email }
+            const result = await enrolledClassCollection.find(query).toArray();
+            res.json(result)
+        })
 
 
 
